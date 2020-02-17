9a732c9c57fe7a73dffe6f4bbf41d1b9e8451da5
jyotsna-deore <61134285+jyotsna-deore@users.noreply.github.com>
2020-02-17 12:34:16 +0530

    Update Demofb.java

--- Githhub project/src/com/abc/Demofb.java
+++ Githhub project/src/com/abc/Demofb.java
@@ -3,10 +3,11 @@ package com.abc;
 public class Demofb {
 public static void main(String[] args) {
 	
-	System.out.println("i ");
+	System.out.println("i");
 	System.out.println("am");
 	System.out.println("gaju");
 	System.out.println("sonone");
+	
 
 }
 }
